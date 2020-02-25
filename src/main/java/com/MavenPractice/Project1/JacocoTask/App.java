93a43d0794ed5f399741123a92724b4943b42b3e
Suni Sara Varkey <sunisaravarkey@gmail.com>
2020-02-25 11:00:37 +0530

    Trial Commit

---
+++ src/main/java/com/MavenPractice/Project1/JacocoTask/App.java
@@ -0,0 +1,13 @@
+package com.MavenPractice.Project1.JacocoTask;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
