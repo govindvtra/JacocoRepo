a13ba080b510b2f977e72bd97720db008ac24c69
Suni Sara Varkey <sunisaravarkey@gmail.com>
2020-02-26 12:37:56 +0530

    Jacoco Task file added

--- src/main/java/com/MavenPractice/Project1/JacocoTask/App.java
+++ src/main/java/com/MavenPractice/Project1/JacocoTask/App.java
@@ -9,5 +9,18 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+    }
+    public int add(int a,int b) {
+    	return(a+b);
+    }
+    public int sub(int a,int b) {
+    	return(a-b);
+    }
+    public int mul(int a,int b) {
+    	return(a*b);
+    }
+    public int rem(int a,int b) {
+    	return(a%b);
     }
 }
--- src/test/java/com/MavenPractice/Project1/JacocoTask/AppTest.java
+++ src/test/java/com/MavenPractice/Project1/JacocoTask/AppTest.java
@@ -1,5 +1,6 @@
 package com.MavenPractice.Project1.JacocoTask;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -12,9 +13,28 @@ public class AppTest
     /**
      * Rigorous Test :-)
      */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+	@Test
+	public void addtest() {
+		App obj1= new App();
+		int result= obj1.add(3,4);
+		assertEquals(7,result);
+	}
+	@Test
+	public void subtest() {
+		App obj1= new App();
+		int result= obj1.sub(4,3);
+		assertEquals(1,result);
+	}
+	@Test
+	public void multest() {
+		App obj1= new App();
+		int result= obj1.mul(3,4);
+		assertEquals(12,result);
+	}
+	@Test
+	public void remtest() {
+		App obj1= new App();
+		int result= obj1.rem(4,3);
+		assertEquals(1,result);
+	}
 }
